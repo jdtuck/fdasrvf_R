dc37d84fd1c6bb6612c42516f40a958bb202944b
Derek Tucker <james.d.tucker@navy.mil>
2012-12-26 11:07:58 -0600

    CRAN fixes

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -332,7 +332,6 @@ svd2 <- function (x, nu = min(n, p), nv = min(n, p), LINPACK = FALSE)
 		if ((nNA > 0) || (nInf > 0)) {
 			msg <- paste("sum(is.na(x)) = ", nNA, "; sum(abs(x)==Inf) = ", 
 									 nInf, ".  'x stored in .svd.x.NA.Inf'", sep = "")
-			assign(".svd.x.NA.Inf", x, envir = .GlobalEnv)
 			stop(msg)
 		}
 		attr(x, "n") <- n
@@ -340,17 +339,13 @@ svd2 <- function (x, nu = min(n, p), nv = min(n, p), LINPACK = FALSE)
 		attr(x, "LINPACK") <- LINPACK
 		.x2 <- c(".svd.LAPACK.error.matrix", ".svd.LINPACK.error.matrix")
 		.x <- .x2[1 + LINPACK]
-		assign(.x, x, envir = .GlobalEnv)
 		msg <- paste("svd failed using LINPACK = ", LINPACK, 
-								 " with n = ", n, " and p = ", p, ";  x stored in '", 
-								 .x, "'", sep = "")
+								 " with n = ", n, " and p = ", p, ";", sep = "")
 		warning(msg)
 		svd.x <- try(svd(x, nu, nv, !LINPACK))
 		if (class(svd.x) == "try-error") {
 			.xc <- .x2[1 + (!LINPACK)]
-			assign(.xc, x, envir = .GlobalEnv)
-			stop("svd also failed using LINPACK = ", !LINPACK, 
-					 ";  x stored in '", .xc, "'")
+			stop("svd also failed using LINPACK = ", !LINPACK)
 		}
 	}
 	svd.x
