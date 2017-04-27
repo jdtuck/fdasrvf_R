337f5ece5961e31cf921787faeab6e53ec3852ea
Derek Tucker <jdtuck@sandia.gov>
2017-04-27 14:35:18 -0600

    expand amplitudebox surface plot to plot alpha quantiles

--- R/AmplitudeBoxplot.R
+++ R/AmplitudeBoxplot.R
@@ -138,7 +138,6 @@ AmplitudeBoxplot <- function(fn, fmedian, qn, qmedian, time, alpha=.05, ka=1,
   maxx <- fn[,max_index]
 
   if (showplot){
-    colors <- viridis(6)
     ymin <- min(c(min(fmedian),min(Q1),min(Q3),min(maxx),min(minn)))
     ymax <- max(c(max(fmedian),max(Q1),max(Q3),max(maxx),max(minn)))
     plot(time, fmedian, col="black",xlab="Time",main="Amplitude Boxplot", type="l", ylim=c(ymin, ymax))
@@ -150,31 +149,39 @@ AmplitudeBoxplot <- function(fn, fmedian, qn, qmedian, time, alpha=.05, ka=1,
     lines(time, minn, col="red")
 
     s <- seq(0,1,length.out=100)
-    Fs2 <- matrix(0,length(time), 397)
+    Fs2 <- matrix(0,length(time), 595)
     Fs2[,1] <- (1-s[1]) * minn + s[1] * Q1
     for (j in 2:100){
-      Fs2[,j] <- (1-s[j]) * minn + s[j] * Q1
-      Fs2[,99+j] <- (1-s[j]) * Q1 + s[j] * fmedian
-      Fs2[,198+j] <- (1-s[j]) * fmedian + s[j] * Q3
-      Fs2[,297+j] <- (1-s[j]) * Q3 + s[j] * maxx
+      Fs2[,j] <- (1-s[j]) * minn + s[j] * Q1a
+      Fs2[,99+j] <- (1-s[j]) * Q1a + s[j] * Q1
+      Fs2[,198+j] <- (1-s[j]) * Q1 + s[j] * fmedian
+      Fs2[,297+j] <- (1-s[j]) * fmedian + s[j] * Q3
+      Fs2[,396+j] <- (1-s[j]) * Q3 + s[j] * Q3a
+      Fs2[,495+j] <- (1-s[j]) * Q3a + s[j] * maxx
     }
     d1<-sqrt(trapz(time,(qmedian-Q1_q)^2))
-    dl<-sqrt(trapz(time,(Q1_q-min_q)^2))
+    d1a<-sqrt(trapz(time,(Q1_q-Q1a_q)^2))
+    dl<-sqrt(trapz(time,(Q1a_q-min_q)^2))
     d3<-sqrt(trapz(time,(qmedian-Q3_q)^2))
-    du<-sqrt(trapz(time,(Q3_q-max_q)^2))
-    part1<-seq(-d1-dl,-d1,length.out=100)
-    part2<-seq(-d1,0,length.out=100)
-    part3<-seq(0,d3,length.out=100)
-    part4<-seq(d3,d3+du,length.out=100)
-    allparts<-c(part1,part2[2:100],part3[2:100],part4[2:100])
+    d3a<-sqrt(trapz(time,(Q3_q-Q3a_q)^2))
+    du<-sqrt(trapz(time,(Q3a_q-max_q)^2))
+    part1<-seq(-d1-d1a-dl,-d1-d1a,length.out=100)
+    part2<-seq(-d1-d1a,-d1,length.out=100)
+    part3<-seq(-d1,0,length.out=100)
+    part4<-seq(0,d3,length.out=100)
+    part5<-seq(d3,d3+d3a,length.out=100)
+    part6<-seq(d3+d3a,d3+d3a+du,length.out=100)
+    allparts<-c(part1,part2[2:100],part3[2:100],part4[2:100],part5[2:100],part6[2:100])
 
     if (requireNamespace("plot3Drgl", quietly = TRUE)) {
       p=persp3D(x=time,y=allparts,z=Fs2,col=viridis(128),plot=F,main="Amplitude Surface Plot",ticktype="detailed",box=F)+
         lines3D(x=time,y=rep(0,M),z=fmedian,col="black",lwd=6,add=T,plot=F)+
         lines3D(x=time,y=rep(-d1,M),z=Q1,col="blue",lwd=6,add=T,plot=F)+
-        lines3D(x=time,y=rep(-d1-dl, M),z=minn,col="red",lwd=6,add=T,plot=F)+
+        lines3D(x=time,y=rep(-d1-d1a,M),z=Q1a,col="green",lwd=6,add=T,plot=F)+
+        lines3D(x=time,y=rep(-d1-d1a-dl, M),z=minn,col="red",lwd=6,add=T,plot=F)+
         lines3D(x=time,y=rep(d3, M),z=Q3,col="blue",lwd=6,add=T,plot=F)+
-        lines3D(x=time,y=rep(d3+du, M),z=maxx,col="red",lwd=6,add=T,plot=F)
+        lines3D(x=time,y=rep(d3+d3a, M),z=Q3a,col="green",lwd=6,add=T,plot=F)+
+        lines3D(x=time,y=rep(d3+d3a+du, M),z=maxx,col="red",lwd=6,add=T,plot=F)
       plotrgl()
       par3d("windowRect"= c(0,0,640,640))
       grid3d(c("x", "y+", "z"))
@@ -184,9 +191,11 @@ AmplitudeBoxplot <- function(fn, fmedian, qn, qmedian, time, alpha=.05, ka=1,
       image(time, allparts, Fs2, main="Surface Plot", ylab="", col=viridis(128))
       lines(time, rep(0, M), col="black", lwd=1)
       lines(time, rep(-d1, M), col="blue", lwd=1)
-      lines(time, rep(-d1-dl, M), col="red", lwd=1)
+      lines(time, rep(-d1-d1a, M), col="green", lwd=1)
+      lines(time, rep(-d1-d1a-dl, M), col="red", lwd=1)
       lines(time, rep(d3, M), col="blue", lwd=1)
-      lines(time, rep(d3+du, M), col="red", lwd=1)
+      lines(time, rep(d3+d3a, M), col="green", lwd=1)
+      lines(time, rep(d3+d3a+du, M), col="red", lwd=1)
     }
 
 
