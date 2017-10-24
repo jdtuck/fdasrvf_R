eaf2cf55b94896bf1f8574f138fadf7b40a0628e
afoss <afoss@s1013356-card2.mdl.sandia.gov>
2017-10-24 12:41:16 -0600

    Add unit testing for pair_align_functions_bayes

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(fdasrvf)
+
+test_check("fdasrvf")
---
+++ tests/testthat/test_pair_align_functions_bayes.R
@@ -0,0 +1,20 @@
+library(fdasrvf)
+context("Pair align functions bayes Cheng")
+
+test_that("pair_align_functions_bayes() works as intended", {
+  set.seed(1)
+  data('simu_data')
+  out <- pair_align_functions_bayes(f1 = simu_data$f[,1], f2 = simu_data$f[,2],
+    timet = simu_data$time, iter = 5000, times = 5, tau = 2, powera = 1,
+    showplot = FALSE, extrainfo = TRUE)
+  # verify functions match approximately
+  expect_equal(sum(simu_data$f[,1] - out$f2_a), -10.1946817)
+  expect_equal(sum(simu_data$f[,1] - out$f2_q), -10.0712115)
+  # verify warning issued for times = 2
+  expect_warning(pair_align_functions_bayes(f1 = simu_data$f[,1],
+    f2 = simu_data$f[,2], timet = simu_data$time, iter = 1000, times = 2,
+    tau = 2, powera = 1, showplot = FALSE, extrainfo = FALSE))
+  # verify acceptance rate
+  expect_equal(mean(out$pct_accept), 0.0304315789)
+})
+
