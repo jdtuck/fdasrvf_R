1e0ccbc4c603a46e69d5ac15f2ae73ab7301497c
Derek Tucker <dtucker@Dereks-iMac.local>
2020-02-15 13:50:29 -0700

    fix LTO dgemv, types are different between armadillo

--- src/gropt/incl/dgemv.h
+++ src/gropt/incl/dgemv.h
@@ -3,9 +3,9 @@ extern "C" {
 #endif  
 
 #include "f2c.h" 
-#define dgemv1_  dgemv_
+#define dgemv1_  dgemv
 
-int dgemv_(char *trans, integer *m, integer *n, doublereal *alpha, doublereal *a, integer *lda, doublereal *x, integer *incx, doublereal *beta, doublereal *y, integer *incy, int trans_len);
+int dgemv(char *trans, integer *m, integer *n, doublereal *alpha, doublereal *a, integer *lda, doublereal *x, integer *incx, doublereal *beta, doublereal *y, integer *incy, int trans_len);
 
 #ifdef __cplusplus
 }
