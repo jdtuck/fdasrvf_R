33562a8a3ef8cc393321ab051e76c5fa4b1f7c85
J. Derek Tucker <jdtuck@sandia.gov>
2021-01-21 08:16:25 -0700

    update shape distance to matlab version

--- R/calc_shape_dist.R
+++ R/calc_shape_dist.R
@@ -5,15 +5,40 @@
 #' @param beta1 array describing curve1 (n,T)
 #' @param beta2 array describing curve
 #' @param mode Open ("O") or Closed ("C") curves
-#' @return d geodesic distance
-#' @keywords srvf alignment
+#' @return Returns a list containing \item{d}{geodesic distance}
+#' \item{dx}{phase distance}
+#' @keywords distances
 #' @references Srivastava, A., Klassen, E., Joshi, S., Jermyn, I., (2011). Shape analysis of elastic curves in euclidean spaces. Pattern Analysis and Machine Intelligence, IEEE Transactions on 33 (7), 1415-1428.
 #' @export
 #' @examples
 #' data("mpeg7")
-#' d = calc_shape_dist(beta[,,1,1],beta[,,1,4])
+#' out = calc_shape_dist(beta[,,1,1],beta[,,1,4])
 calc_shape_dist <- function(beta1, beta2, mode="O"){
-    out = inverse_exp_coord(beta1, beta2, mode)
+    T1 = ncol(beta1)
+    centroid1 = calculatecentroid(beta1)
+    dim(centroid1) = c(length(centroid1),1)
+    beta1 = beta1 - repmat(centroid1,1,T1)
+    q1 = curve_to_q(beta1)
 
-    return(out$dist)
+    centroid1 = calculatecentroid(beta2)
+    dim(centroid1) = c(length(centroid1),1)
+    beta2 = beta2 - repmat(centroid1,1,T1)
+
+    out = find_rotation_seed_coord(beta1, beta2, mode)
+    q1dotq2 = innerprod_q2(q1, out$q2best)
+
+    if (q1dotq2 > 1){
+        q1dotq2 = 1
+    }
+
+    d = acos(q1dotq2)
+
+    gam = out$gambest
+    time1 <- seq(0,1,length.out=T1)
+    binsize <- mean(diff(time1))
+    psi <- sqrt(gradient(gam,binsize))
+    v <- inv_exp_map(rep(1,length(gam)), psi)
+    dx <- sqrt(trapz(time1, v^2))
+
+    return(list(d=d,dx=dx))
 }
