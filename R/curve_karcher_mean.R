9a8a3fbe9ef759bd02ec2a052aad7c90e256668b
J. Derek Tucker <jdtuck@sandia.gov>
2021-01-20 17:58:47 -0700

    bring R curve up to date with matlab for mean computation

--- R/curve_karcher_mean.R
+++ R/curve_karcher_mean.R
@@ -23,24 +23,27 @@ curve_karcher_mean <- function (beta, mode = "O", rotated = T, maxit = 20, ms = 
 {
     if(ms!="mean"&ms!="median"){warning("ms must be either \"mean\" or \"median\". ms has been set to \"mean\"",immediate. = T)}
     if(ms!="median"){ms = "mean"}
+
     tmp = dim(beta)
     n = tmp[1]
     T1 = tmp[2]
     N = tmp[3]
     q = array(0, c(n, T1, N))
     for (ii in 1:N) {
-        q[, , ii] = curve_to_q(beta[, , ii])
+        beta1 = beta[,,ii]
+        centroid1 = calculatecentroid(beta1)
+        dim(centroid1) = c(length(centroid1),1)
+        beta1 = beta1 - repmat(centroid1,1,T1)
+        q[, , ii] = curve_to_q(beta1)
     }
-    mnq = rowMeans(q[1, , ])
-    dqq = sqrt(colSums((q[1, , ] - matrix(mnq, ncol = N, nrow = T1))^2))
-    min_ind = which.min(dqq)
-    mu = q[, , min_ind]
-    betamean = beta[, , min_ind]
+
+    mu = q[, , 1]
     delta = 0.5
     tolv = 1e-04
     told = 5 * 0.001
     itr = 1
     sumd = rep(0, maxit + 1)
+    sumd[1] = Inf
     v = array(0, c(n, T1, N))
     normvbar = rep(0, maxit + 1)
     if(ms == "median"){ #run for median only, saves memory if getting mean
@@ -50,17 +53,49 @@ curve_karcher_mean <- function (beta, mode = "O", rotated = T, maxit = 20, ms = 
     while (itr < maxit) {
         cat(sprintf("Iteration: %d\n", itr))
         mu = mu/sqrt(innerprod_q2(mu, mu))
-        
+
+        if (mode=="C"){
+            basis = find_basis_normal(mu)
+        }
+
         for (i in 1:N) {
-            out = karcher_calc(beta[, , i], q[, , i], betamean, 
-                               mu, rotated, mode)
-            v[, , i] = out$v
+            q1 = q[, , i]
+
+            out = find_rotation_seed_unqiue(mu,q1,mode)
+            qn_t = out$q2best/sqrt(innerprod_q2(out$q2best,out$q2best))
+
+            q1dotq2 = innerprod_q2(mu,qn_t)
+
+            if (q1dotq2 > 1){
+                q1dotq2 = 1
+            }
+            if (q1dotq2 < -1){
+                q1dotq2 = -1
+            }
+                
+            dist = acos(q1dotq2)
+
+            u = qn_t - q1dotq2 * q1
+            normu = sqrt(innerprod_q2(u,u))
+            if (normu > 1e-4){
+                w = u*acos(q1dotq2)/normu
+            } else {
+                w = matrix(0, nrow(beta1), T1)
+            }
+
+            if (mode=="O"){
+                v[, , i] = w
+            } else {
+                v[, , i] = project_tangent(w, q1, basis)
+            }
+
             if(ms == "median"){ #run for median only, saves computation time if getting mean
                 d_i[i] = sqrt(innerprod_q2(v[,,i], v[,,i])) #calculate sqrt of norm of v_i
                 v_d[,,i] = v[,,i]/d_i[i] #normalize v_i
             }
-            sumd[itr + 1] = sumd[itr + 1] + out$d^2
+            sumd[itr + 1] = sumd[itr + 1] + dist^2
         }
+
         if(ms == "median"){#run for median only
             sumv = rowSums(v_d, dims = 2)
             sum_dinv = sum(1/d_i)
@@ -70,12 +105,13 @@ curve_karcher_mean <- function (beta, mode = "O", rotated = T, maxit = 20, ms = 
             sumv = rowSums(v, dims = 2)
             vbar = sumv/N
         }
+
         normvbar[itr] = sqrt(innerprod_q2(vbar, vbar))
         normv = normvbar[itr] 
         if ((normv > tolv) && (abs(sumd[itr + 1] - sumd[itr]) > 
                                told)) {
             mu = cos(delta * normvbar[itr]) * mu + sin(delta * 
-                                                           normvbar[itr]) * vbar/normvbar[itr]
+                     normvbar[itr]) * vbar/normvbar[itr]
             if (mode == "C") {
                 mu = project_curve(mu)
             }
