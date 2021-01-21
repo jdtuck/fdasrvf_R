dde4db5b30d59bd36185c59cdf8d76ed0fa6303b
J. Derek Tucker <jdtuck@sandia.gov>
2021-01-21 09:06:24 -0700

    update principal directions to match MATLAB

--- R/curve_principal_directions.R
+++ R/curve_principal_directions.R
@@ -2,115 +2,57 @@
 #'
 #' Calculate principal directions of a set of curves
 #'
-#' @param betamean array (n,T) of mean curve
-#' @param mu array (n,T) of mean srvf
+#' @param v array (n,T) of shooting vectors
 #' @param K array (2*T,2*T) covariance matrix
-#' @param mode Open ("O") or Closed ("C") curves
+#' @param mu array (n,T) of mean srvf
 #' @param no number of components
 #' @param N number of samples on each side of mean
-#' @return pd list describing principal directions
+#' @return Returns a list containing \item{s}{singular values}
+#' \item{U}{singular vectors}
+#' \item{coef}{principal coefficients}
+#' \item{pd}{principal directions}
 #' @keywords srvf alignment
 #' @references Srivastava, A., Klassen, E., Joshi, S., Jermyn, I., (2011). Shape analysis of elastic curves in euclidean spaces. Pattern Analysis and Machine Intelligence, IEEE Transactions on 33 (7), 1415-1428.
 #' @export
 #' @examples
 #' data("mpeg7")
-#' out = curve_srvf_align(beta[,,1,1:2],maxit=2) # note: use more shapes, small for speed
-#' K = curve_karcher_cov(out$betamean, beta[,,1,1:2])
-#' pd = curve_principal_directions(out$betamean, out$q_mu, K)
-curve_principal_directions <- function(betamean, mu, K, mode="O", no=3, N=5){
-    n = nrow(betamean)
-    T1 = ncol(betamean)
+#' out = curve_karcher_mean(beta[,,1,1:2], maxit=2) # note: use more shapes, small for speed
+#' K = curve_karcher_cov(out$betamean, out$v)
+#' out = curve_principal_directions(out$v, K, out$mu)
+curve_principal_directions <- function(v, K, mu, no=3, N=5){
+    n = nrow(mu)
+    T1 = ncol(mu)
+
+    # SVD
     out = svd(K)
-    U = out$u
-    s = out$d
-    V = out$v
+    U = out$u[,1:no]
+    s = out$d[1:no]
 
-    qarray = array(list(), c(no,2*N+1))
-    qarray1 = array(list(), N)
-    qarray2 = array(list(), N)
-    pd = array(list(), c(no, 2*N+1))
-    pd1 = array(list(), N)
-    pd2 = array(list(), N)
+    # express shapes as coefficients
+    tmp = dim(v)
+    N = tmp[3]
+    VM = apply(v, c(1,2), mean)
+    VM = c(VM)
+    x = matrix(0, no, N)
+    for (ii in 1:N){
+        tmpv = v[, , ii]
+        x[, ii] = t(U)%*%(c(tmpv)-VM)
+    }
 
+
+    pd = array(list(), c(no, N))
     for (m in 1:no){
-        princDir = rbind(U[1:T1,m], U[(T1+1):(2*T1),m])
-        v = sqrt(s[m])*princDir
-        q1 = mu
-        epsilon = 2./N
-
-        # forward direction from mean
         for (i in 1:N){
-            normv = sqrt(innerprod_q2(v,v))
+            tmp = VM + 0.5*(i-5)*sqrt(s[m])*U[,m]
+            v1 = tmp
+            dim(v1) = c(n,T1)
+            q2n = elastic_shooting(mu, v1)
+            p = q_to_curve(q2n)
 
-            if (normv < 1e-4){
-                q2 = mu
-            } else {
-                q2 = cos(epsilon*normv)*q1 + sin(epsilon*normv)*v/normv
-                if (mode=="C"){
-                    q2 = project_curve(q2)
-                }
-            }
+            pd[m, i][[1]] = p
 
-            qarray1[[i]] = q2
-            p = q_to_curve(q2)
-            centroid1 = -1 * calculatecentroid(p)
-            dim(centroid1) = c(length(centroid1),1)
-            out = scale_curve(p+repmat(centroid1,1,T1))
-            pd1[[i]] = out$beta_scaled
-
-            # Parallel translate tangent vector
-            basis2 = find_basis_normal(q2)
-            v = parallel_translate(v, q1, q2, basis2, mode)
-
-            q1 = q2
-        }
-
-        # Backward direction from mean
-        v = -sqrt(s[m])*princDir
-        q1 = mu
-        for (i in 1:N){
-            normv = sqrt(innerprod_q2(v,v))
-
-            if (normv < 1e-4){
-                q2 = mu
-            } else {
-                q2 = cos(epsilon*normv)*q1 + sin(epsilon*normv)*v/normv
-                if (mode=="C"){
-                    q2 = project_curve(q2)
-                }
-            }
-
-            qarray2[[i]] = q2
-            p = q_to_curve(q2)
-            centroid1 = -1*calculatecentroid(p)
-            dim(centroid1) = c(length(centroid1),1)
-            out = scale_curve(p+repmat(centroid1, 1, T1))
-            pd2[[i]] = out$beta_scaled
-
-            # parallel translate tangent vector
-            basis2 = find_basis_normal(q2)
-            v = parallel_translate(v, q1, q2, basis2, mode)
-
-            q1 = q2
-        }
-
-        for (i in 1:N){
-            qarray[[m,i]] = qarray2[[N+1-i]]
-            pd[[m,i]] = pd2[[N+1-i]]
-        }
-
-        qarray[[m,N+1]] = mu
-        centroid1 = -1 * calculatecentroid(betamean)
-        dim(centroid1) = c(length(centroid1),1)
-        out = scale_curve(betamean + repmat(centroid1,1,T1))
-
-        pd[[m,N]] = out$beta_scaled
-
-        for (i in 1:N){
-            qarray[[m,i+N+1]] = qarray1[[i]]
-            pd[[m,i+N+1]] = pd1[[i]]
         }
     }
 
-    return(pd)
+    return(list(s = s, U = U, coef = x, pd = pd, VM=VM))
 }
