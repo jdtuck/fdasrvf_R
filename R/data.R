52a633775bd564025d331523fd88fcadc7282db2
Derek Tucker <jdtuck@users.noreply.github.com>
2023-02-17 21:54:42 -0700

    Update data.R

--- R/data.R
+++ R/data.R
@@ -18,7 +18,6 @@
 #' - `time`: A numeric vector of size \eqn{M = 101} storing the grid on which
 #' the curves `f` have been evaluated.
 #'
-#' @source ???
 "simu_data"
 
 #' Distributed Gaussian Peak Dataset
@@ -36,7 +35,6 @@
 #' - `time`: A numeric vector of size \eqn{M = 101} storing the grid on which
 #' the curves `f` have been evaluated.
 #'
-#' @source ???
 "toy_data"
 
 #' Berkeley Growth Velocity Dataset
@@ -51,7 +49,6 @@
 #' - `time`: A numeric vector of size \eqn{M = 69} storing the grid on which
 #' the curves `f` have been evaluated.
 #'
-#' @source ???
 "growth_vel"
 
 #' Aligned Distributed Gaussian Peak Dataset
@@ -65,7 +62,6 @@
 #' A list which contains the output of the [time_warping()] function applied on
 #' the data set `toy_data`.
 #'
-#' @source ???
 "toy_warp"
 
 #' Aligned Simulated two Gaussian Dataset
@@ -84,7 +80,6 @@
 #' A list which contains the output of the [time_warping()] function applied on
 #' the data set `simu_data`.
 #'
-#' @source ???
 "simu_warp"
 
 #' Aligned Simulated two Gaussian Dataset using Median
@@ -103,7 +98,6 @@
 #' A list which contains the output of the [time_warping()] function finding the
 #' median applied on the data set `simu_data`.
 #'
-#' @source ???
 "simu_warp_median"
 
 #' MPEG7 Curve Dataset
@@ -112,10 +106,9 @@
 #'
 #' @format ## `beta`
 #' An array of shape \eqn{2 \times 100 \times 65 \times 20} storing a sample of
-#' \eqn{20} curves from \eqn{\mathbb{R}} to \eqn{\mathbb{R}^2} distributed in
+#' \eqn{20} curves from \eqn{R} to \eqn{R^2} distributed in
 #' \eqn{65} different classes, evaluated on a grid of size \eqn{100}.
 #'
-#' @source ???
 "beta"
 
 #' Example Image Data set
@@ -128,5 +121,4 @@
 #' - `I1`: A numeric matrix of shape \eqn{64 \times 64} storing the 1st image;
 #' - `I2`: A numeric matrix of shape \eqn{64 \times 64} storing the 2nd image.
 #'
-#' @source ???
 "im"
