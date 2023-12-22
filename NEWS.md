ce7e99a4830db509e0e93bec25282ddfff20e809
J. Derek Tucker <jdtuck@sandia.gov>
2023-12-22 05:50:34 -0700

    remove multidimensional case as it is covered more correctly in curve_to_q

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,6 @@
+# fdasrvf 2.1.x
+* bugfixes
+
 # fdasrvf 2.1.2
 * add rbfgs c++ code
 
--- R/f_to_srvf.R
+++ R/f_to_srvf.R
@@ -1,35 +1,25 @@
-#' Transformation to SRSF Space
+#' Transformation to SRVF Space
 #'
-#' This function transforms curves from their original functional space to the
-#' SRVF space.
+#' This function transforms functions in R^1 from their original functional
+#' space to the SRVF space.
 #'
 #' @param f Either a numeric vector of a numeric matrix or a numeric array
 #'   specifying the functions that need to be transformed.
 #'
 #'   - If a vector, it must be of shape \eqn{M} and it is interpreted as a
 #'   single \eqn{1}-dimensional curve observed on a grid of size \eqn{M}.
-#'   - If a matrix and `multidimensional == FALSE`, it must be of shape
+#'   - If a matrix, it must be of shape
 #'   \eqn{M \times N}. In this case, it is interpreted as a sample of \eqn{N}
 #'   curves observed on a grid of size \eqn{M}, unless \eqn{M = 1} in which case
 #'   it is interpreted as a single \eqn{1}-dimensional curve observed on a grid
 #'   of size \eqn{M}.
-#'   - If a matrix and `multidimensional == TRUE`,it must be of shape
-#'   \eqn{L \times M} and it is interpreted as a single \eqn{L}-dimensional
-#'   curve observed on a grid of size \eqn{M}.
-#'   - If a 3D array, it must be of shape \eqn{L \times M \times N} and it is
-#'   interpreted as a sample of \eqn{N} \eqn{L}-dimensional curves observed on a
-#'   grid of size \eqn{M}.
 #' @param time A numeric vector of length \eqn{M} specifying the grid on which
-#'   the curves are evaluated.
-#' @param multidimensional A boolean specifying if the curves are
-#'   multi-dimensional. This is useful when `f` is provided as a matrix to
-#'   determine whether it is a single multi-dimensional curve or a collection of
-#'   uni-dimensional curves. Defaults to `FALSE`.
+#'   the functions are evaluated.
 #'
 #' @return A numeric array of the same shape as the input array `f` storing the
-#'   SRSFs of the original curves.
+#'   SRVFs of the original curves.
 #'
-#' @keywords srsf alignment
+#' @keywords srvf alignment
 #'
 #' @references Srivastava, A., Wu, W., Kurtek, S., Klassen, E., Marron, J. S.,
 #'   May 2011. Registration of functional data using Fisher-Rao metric,
@@ -41,10 +31,10 @@
 #' @export
 #' @examples
 #' q <- f_to_srvf(simu_data$f, simu_data$time)
-f_to_srvf <- function(f, time, multidimensional = FALSE) {
+f_to_srvf <- function(f, time) {
   binsize <- mean(diff(time))
   eps <- .Machine$double.eps
-  g <- gradient(f, binsize, multidimensional = multidimensional)
+  g <- gradient(f, binsize)
   # compute norm of g
   dims <- dim(g)
   if (is.null(dims)) {
@@ -54,30 +44,11 @@ f_to_srvf <- function(f, time, multidimensional = FALSE) {
     N <- 1
     norm_g <- abs(g)
   } else if (length(dims) == 2) {
-    if (multidimensional || dims[1] == 1) {
-      # g is a single multidimensional curve (LxM)
-      L <- dims[1]
-      M <- dims[2]
-      N <- 1
-      norm_g <- matrix(sqrt(colSums(g^2)), nrow = L, ncol = M, byrow = TRUE)
-    } else {
       # g is a collection of unidimensional curves (MxN)
       L <- 1
       M <- dims[1]
       N <- dims[2]
       norm_g <- abs(g)
-    }
-  } else {
-    # g is a collection of multidimensional curves (LxMxN)
-    L <- dims[1]
-    M <- dims[2]
-    N <- dims[3]
-    norm_g_list <- lapply(1:N, function(n) {
-      sqrt(colSums(g[, , n, drop = FALSE]^2))
-    })
-    norm_g <- array(dim = c(L, M , N))
-    for (n in 1:N)
-      norm_g[, , n] <- matrix(norm_g_list[[n]], nrow = L, ncol = M, byrow = TRUE)
   }
 
   g / sqrt(norm_g + eps)
