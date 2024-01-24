92c6099a8a3cae9630d246cdbdad36efdf2678c2
J. Derek Tucker <jdtuck@sandia.gov>
2024-01-24 09:28:54 -0700

    fix s3 methods

--- R/predict.hfpca.R
+++ R/predict.hfpca.R
@@ -1,30 +1,45 @@
-#' @export
-predict.hfpca <- function(x, f, ...){
-  q1 = f_to_srvf(f, x$warp_data$time)
-  M = length(x$warp_data$time)
-  N = ncol(f)
+#' Elastic Prediction for functional PCA
+#'
+#' This function performs projection of new functions on fPCA basis
+#'
+#' @param object Object of class inheriting from "horizFPCA"
+#' @param newdata An optional matrix in which to look for functions with which to predict. If omitted, the original functions are used.
+#' @param ... additional arguments affecting the predictions produced
+#' @return Returns a matrix
+#' \item{a}{principle coefficients}
+#' @keywords srvf alignment regression
+#' @references Tucker, J. D., Wu, W., Srivastava, A.,
+#'  Generative Models for Function Data using Phase and Amplitude Separation,
+#'  Computational Statistics and Data Analysis (2012), 10.1016/j.csda.2012.12.001.
+predict.hfpca <- function(object, newdata=NULL, ...){
+  if (is.null(newdata)){
+    newdata = object$warp_data$f0
+  }
+  q1 = f_to_srvf(newdata, object$warp_data$time)
+  M = length(object$warp_data$time)
+  N = ncol(newdata)
   gam = matrix(0,M,N)
   fn = matrix(0,M,N)
   qn = matrix(0,M,N)
   for (ii in 1:N){
-    gam[,ii] = optimum.reparam(x$warp_data$mqn, x$warp_data$time, q1[,ii],
-                               x$warp_data$time, method=x$warp_data$call$optim_method)
+    gam[,ii] = optimum.reparam(object$warp_data$mqn, object$warp_data$time, q1[,ii],
+                               object$warp_data$time, method=object$warp_data$call$optim_method)
   }
 
 
-  no = ncol(x$U)
+  no = ncol(object$U)
   psi = matrix(0,M,N)
   vec = matrix(0,M,N)
-  binsize <- mean(diff(x$warp_data$time))
+  binsize <- mean(diff(object$warp_data$time))
   for (i in 1:N){
     psi[,i] = sqrt(gradient(gam[,i],binsize))
-    vec[,i] <- inv_exp_map(x$mu, psi[,i])
+    vec[,i] <- inv_exp_map(object$mu, psi[,i])
   }
 
   a <- matrix(0,N,no)
   for (i in 1:N){
     for (j in 1:no){
-      a[i,j] <- (vec[,i]-x$vm)%*%x$U[,j]
+      a[i,j] <- (vec[,i]-object$vm)%*%object$U[,j]
     }
   }
 
--- R/predict.vfpca.R
+++ R/predict.vfpca.R
@@ -1,27 +1,42 @@
-#' @export
-predict.vfpca <- function(x, f, ...){
-  q1 = f_to_srvf(f, x$warp_data$time)
-  M = length(x$warp_data$time)
-  N = ncol(f)
+#' Elastic Prediction for functional PCA
+#'
+#' This function performs projection of new functions on fPCA basis
+#'
+#' @param object Object of class inheriting from "vertFPCA"
+#' @param newdata An optional matrix in which to look for functions with which to predict. If omitted, the original functions are used.
+#' @param ... additional arguments affecting the predictions produced
+#' @return Returns a matrix
+#' \item{a}{principle coefficients}
+#' @keywords srvf alignment regression
+#' @references Tucker, J. D., Wu, W., Srivastava, A.,
+#'  Generative Models for Function Data using Phase and Amplitude Separation,
+#'  Computational Statistics and Data Analysis (2012), 10.1016/j.csda.2012.12.001.
+predict.vfpca <- function(object, newdata=NULL, ...){
+  if (is.null(newdata)){
+    newdata = object$warp_data$f0
+  }
+  q1 = f_to_srvf(newdata, object$warp_data$time)
+  M = length(object$warp_data$time)
+  N = ncol(newdata)
   gam = matrix(0,M,N)
   fn = matrix(0,M,N)
   qn = matrix(0,M,N)
   for (ii in 1:N){
-    gam[,ii] = optimum.reparam(x$warp_data$mqn, x$warp_data$time, q1[,ii],
-                               x$warp_data$time, method=x$warp_data$call$optim_method)
-    fn[,ii] = warp_f_gamma(f[,ii], x$warp_data$time, gam[,ii])
-    qn[,ii] = f_to_srvf(fn[,ii], x$warp_data$time)
+    gam[,ii] = optimum.reparam(object$warp_data$mqn, object$warp_data$time, q1[,ii],
+                               object$warp_data$time, method=object$warp_data$call$optim_method)
+    fn[,ii] = warp_f_gamma(newdata[,ii], object$warp_data$time, gam[,ii])
+    qn[,ii] = f_to_srvf(fn[,ii],object$warp_data$time)
   }
 
-  m_new <- sign(fn[x$id,])*sqrt(abs(fn[x$id,]))  # scaled version
+  m_new <- sign(fn[object$id,])*sqrt(abs(fn[object$id,]))  # scaled version
   qn1 <- rbind(qn,m_new)
 
-  no = ncol(x$U)
+  no = ncol(object$U)
 
   a <- matrix(0,N,no)
   for (i in 1:N){
     for (j in 1:no){
-      a[i,j] <- (qn1[,i]-x$mqn)%*%x$U[,j]
+      a[i,j] <- (qn1[,i]-object$mqn)%*%object$U[,j]
     }
   }
 
