ca5d2d3bf1317609d5898ad991c8c296fc9a493d
J. Derek Tucker <jdtuck@sandia.gov>
2025-06-15 13:59:35 -0600

    add gam_to_psi function

--- R/geometry.R
+++ R/geometry.R
@@ -5,7 +5,7 @@ exp_map <- function(psi, v, wnorm = l2_norm){
   } else {
     expgam <- cos(v_norm) * psi + sin(v_norm) * v / v_norm
   }
-  
+
   return(expgam)
 }
 
@@ -275,6 +275,93 @@ v_to_gam<-function(v){
     for (i in 1:n){
       psi <- exp_map(mu,v[,i])
       gam0 <- cumtrapz(time,psi*psi)
+      gam[,i] <- (gam0 - min(gam0))/(max(gam0)-min(gam0))
+    }
+  }
+  return(gam)
+}
+
+#' map warping function to hilbert sphere
+#'
+#'
+#' @param gam Either a numeric vector of a numeric matrix or a numeric array
+#'   specifying the warping functions
+#' @param smooth Apply smoothing before gradient
+#'
+#' @return A numeric array of the same shape as the input array `gamma` storing the
+#'   shooting vectors of `gamma` obtained via finite differences.
+#'
+#' @keywords srvf alignment
+#' @export
+gam_to_psi<-function(gam, smooth=FALSE){
+  if (ndims(gam) == 0){
+    TT = length(gam)
+    eps = .Machine$double.eps
+    time <- seq(0,1,length.out=TT)
+    binsize <- mean(diff(time))
+
+    psi = rep(0,TT)
+    if (smooth) {
+      tmp.spline <- stats::smooth.spline(gam)
+      g <- stats::predict(tmp.spline, deriv = 1)$y / binsize
+      g[g<0] = 0
+      psi = sqrt(g)
+    } else {
+      psi = sqrt(gradient(gam,binsize))
+    }
+
+  } else {
+    TT = nrow(gam)
+    n = ncol(gam)
+    eps = .Machine$double.eps
+    time <- seq(0,1,length.out=TT)
+    binsize <- mean(diff(time))
+
+    psi = matrix(0,TT,n)
+    if (smooth) {
+      g <- matrix(0, TT, n)
+      for (i in 1:n) {
+        tmp.spline <- stats::smooth.spline(gam[,i])
+        g[, i] <- stats::predict(tmp.spline, deriv = 1)$y / binsize
+        g[g[,i]<0, i] = 0
+        psi[,i] = sqrt(g[, i])
+      }
+    } else {
+      for (i in 1:n){
+        psi[,i] = sqrt(gradient(gam[,i],binsize))
+      }
+    }
+
+  }
+
+  return(psi)
+}
+
+#' map hilbert sphere to warping function
+#'
+#'
+#' @param psi Either a numeric vector of a numeric matrix or a numeric array
+#'   specifying the shooting vectors
+#'
+#' @return A numeric array of the same shape as the input array `psi` storing the
+#'   warping functions `psi`.
+#'
+#' @keywords srvf alignment
+#' @export
+psi_to_gam<-function(v){
+  if (ndims(psi) == 0){
+    TT = length(psi)
+    time <- seq(0,1,length.out=TT)
+    gam0 <- cumtrapz(time,psi*psi)
+    gam <- (gam0 - min(gam0))/(max(gam0)-min(gam0))
+  } else {
+    TT = nrow(psi)
+    n = ncol(psi)
+    time <- seq(0,1,length.out=TT)
+
+    gam = matrix(0,TT,n)
+    for (i in 1:n){
+      gam0 <- cumtrapz(time,psi[,i]*psi[,i])
       gam[,i] <- (gam0 - min(gam0))/(max(gam0)-min(gam0))
     }
   }
