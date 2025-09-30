29e0e43ceff9b9b73e495f4ceeb3ee9d4b97ed2d
J. Derek Tucker <jdtuck@sandia.gov>
2025-09-30 08:12:55 -0600

    start outline of ppd functions

---
+++ R/ppd.R
@@ -0,0 +1,244 @@
+#' Compute Peak Persistance Diagram
+#'
+#' This computes the peak persistance diagram over a range of
+#' lambda. This can help determine the proper elasticity (penalty).
+#' This can be slow and recommended to run in parallel
+#'
+#' @param f A numeric matrix of shape \eqn{M \times N} specifying a sample of
+#'   \eqn{N} curves observed on a grid of size \eqn{M}.
+#' @param time A numeric vector of length \eqn{M} specifying the common grid on
+#'   which all curves `f` have been observed.
+#' @param max_lam maximum value of lambda. Defaults to `2`
+#' @param num_lam number of steps. Defaults to `10`
+#' @param pt the percentile of negative curvature of raw data Defaults to `.15`
+#' @param penalty_method A string specifying the penalty term used in the
+#'   formulation of the cost function to minimize for alignment. Choices are
+#'   `"roughness"` which uses the norm of the second derivative, `"geodesic"`
+#'   which uses the geodesic distance to the identity and `"norm"` which uses
+#'   the Euclidean distance to the identity. Defaults to `"roughness"`.
+#' @param centroid_type A string specifying the type of centroid to align to.
+#'   Choices are `"mean"` or `"median"`. Defaults to `"mean"`.
+#' @param center_warpings A boolean specifying whether to center the estimated
+#'   warping functions. Defaults to `TRUE`.
+#' @param smooth_data A boolean specifying whether to smooth curves using a box
+#'   filter. Defaults to `FALSE`.
+#' @param sparam An integer value specifying the number of times to apply the
+#'   box filter. Defaults to `25L`. This is used only when `smooth_data = TRUE`.
+#' @param parallel A boolean specifying whether to run calculations in parallel.
+#'   Defaults to `TRUE`.
+#' @param cores number of cores in parallel (default=-1, means all cores)
+#' @param optim_method A string specifying the algorithm used for optimization.
+#'   Choices are `"DP"`, `"DPo"`, and `"RBFGS"`. Defaults to `"DP"`.
+#' @param max_iter An integer value specifying the maximum number of iterations.
+#'   Defaults to `20L`.
+#'
+#' @return lam_opt optimal lam
+#'
+#' @keywords srsf alignment
+#' @references Kim, Woo Min, Sutanoy Dasgupta, and Anuj Srivastava. "Peak-persistence diagrams for estimating shapes and functions from noisy data." arXiv preprint arXiv:2305.04826 (2023).
+#'
+#' @export
+#' @examples
+#' \dontrun{
+#'   out <- ppd(simu_data$f, simu_data$time)
+#' }
+ppd <- function(f,
+                time,
+                max_lam = 2,
+                num_lam = 10,
+                pt = 0.15,
+                penalty_method = c("roughness", "geodesic", "norm"),
+                centroid_type = c("mean", "median"),
+                center_warpings = TRUE,
+                smooth_data = FALSE,
+                sparam = 25L,
+                parallel = TRUE,
+                cores = -1,
+                optim_method = c("DP", "DPo", "DP2", "RBFGS"),
+                max_iter = 20L){
+
+  penalty_method <- rlang::arg_match(penalty_method)
+  centroid_type <- rlang::arg_match(centroid_type)
+  optim_method <- rlang::arg_match(optim_method)
+
+  # obtain a lambda candidate set
+  lam_vec <- seq(0, max_lam, length.out = num_lam)
+  fns <- list()
+  for (i in 1:num_lam){
+    obj <- time_warping(f, time, lam_vec[i],
+                        penalty_method, centroid_type, center_warpings,
+                        smooth_data, sparam, parallel, cores,
+                        optim_method, max_iter)
+    fns[[i]] <- obj$fn
+  }
+
+  # peak persistent diagram
+  # get the trehsold for significant peak
+  diff_t = mean(diff(time))
+  taus = list()
+
+  # compute tau values
+  for (i in 1:ncol(f)){
+    idx = findpeaks(f[, i])
+    df2 = gradient(gradient(f[,i], diff_t), diff_t)
+    tau = -df2 / max(-df2)
+    tau[tau < 0] = 0
+    taus[[i]] = tau[idx]
+  }
+  th = quantile(taus, pt)
+
+  obj <- getPPDinfo(time, fns, lam_vec, th)
+  persistent_peak_labels <- getPersistentPeaks(t(obj$IndicatorMatrix))
+
+  # choose optimal lambda
+  ref_row = rep(0, ncol(obj$IndicatorMatrix))
+  ref_row[persistent_peak_labels] <- 1
+
+  n_lams = nrow(obj$IndicatorMatrix)
+  hamming_distances = rep(0, n_lams)
+  exact_match_indices = c()
+
+  for (i in 1:n_lams){
+    comp = !is.nan(IndicatorMatrix[i,])
+
+    # check for exact match
+    if (identical(comp, ref_row)){
+      exact_match_indices = c(exact_match_indices, i)
+    }
+    # calculate Hamming distance
+    hamming_distances[i] = sum(comp != ref_row)
+  }
+
+  if (length(exact_match_indices) > 0){
+    idx_opt = min(exact_match_indices)
+  } else {
+    min_distance_indices = which.min(hamming_distances)
+    idx_opt = min(min_distance_indices)
+  }
+
+  # draw PPD barchart
+  drawPPDBarChart(obj$IndicatorMatrix, obj$Heights, lam_vec, idx_opt)
+  # draw PPD surface
+  if (requireNamespace("plot3Drgl", quietly = TRUE)){
+    drawPPDsurface(time, lam_vec, obj$FNm, obj$Heights, obj$Locs, obj$IndicatorMatrix, obj$Labels, idx_opt)
+  }
+
+  return(lam_vec(idx_opt))
+
+}
+
+getPPDinfo <- function(t, Fa, lam, th){
+
+    n_lams = length(lam)
+
+    # compute the mean of each function in FN over its rows
+    FNm = matrix(0, nrow=length(t), ncol=n_lams)
+    for (i in 1:n_lams){
+        FNm[,i] = rowMeans(Fa[[i]])
+    }
+
+    # find indices of lal maxima in the first function's mean
+    idxMaxFirst = findpeaks(FNm[,1])
+
+    # Initialize Labels and Locations for the first function
+    Labels = list()
+    Locs = list()
+    Labels[[1]] = seq(1, length(idxMaxFirst))
+    Locs[[1]] = idxMaxFirst
+
+    # Initialize the maximum label number
+    labelMax = max(Labels[[1]])
+
+    # process each function to assign labels and locate peaks
+    for (i in 1:(n_lams-1)){
+        currentLabel = Labels[[i]]
+        obj1 = peak_successor(Fa[[i]], Fa[[i+1]], currentLabel, labelMax)
+        Labels[[i+1]] = obj1$Labels
+        labelMax = obj1$labelMax
+
+        # find peak location is the next function's mean
+        idxMaxNext = findpeaks(FNm[, i+1])
+        Locs[[i+1]] = idxMaxNext
+    }
+
+    # preprocess data to compute indicatorMatrix
+    obj2 = PreprocessingForPPD(t, lam , Labels, Locs, labelMax, FNm, th)
+
+    if (all(is.nan(obj2$Heights2))){
+        warning('All peaks are ignored. A smaller threshold is required.')
+    }
+
+    obj$IndicatorMatrix = obj2$IndicatorMatrix
+    obj$Curvatures = obj2$Curvatures
+    obj$Heights = obj2$Heights
+    obj$Locs = Locs
+    obj$Labels = Labels
+    obj$FNm = FNm
+    return(obj)
+
+}
+
+peak_successor <- function(f1, f2, labels1, labelMax){
+
+    # combine f1 and f2 into a 3D array and compute the mean across the second dimension
+    F <- array(c(f1, f2), dim = c(nrow(f1), ncol(f1), 2))
+    fm <- apply(F, c(1, 3), mean)
+
+    # compute peak ranges and labels for fm[,1]
+    obj1 = computePeakRanges(fm[,1])
+    ranges = obj1$ranges
+    idx_max1 = obj1$idx_max
+
+    # compute indices of local maxima in fm[,2]
+    idx_max2 = findpeaks(fm[,2])
+
+    if (length(idx_max1) == 0){ 
+        labels2 = labelMax + seq(1,length(idx_max2))
+    } else {
+        # assign labels to peaks in fm[,2] based on matching ranges in fm[,1]
+        labels2 = assignLabelsToPeaks(idx_max2, ranges, idx_max1, labels1)
+
+        # ensure no overlapping labels in labels2
+        labels2 = resolveOverlappingLabels(labels2, idx_max1, idx_max2, labels1)
+
+        # assign new labels to unmatched peaks in fm[, 2]
+        unmatched = labels2 == 0
+        labels2[unmatched] = labelMax + seq(1,sum(unmatched))
+        labelMax = labelMax + sum(unmatched)
+    }
+
+    obj$labels2 = labels2
+    obj$labelMax = labelMax
+    return(obj)
+}
+
+computePeakRanges <- function(data){
+
+    obj$ranges = ranges
+    obj$idx_max = idx_max
+    return(obj)
+}
+
+assignLabelsToPeaks <- function(){
+
+}
+
+resolveOverlappingLabels <- function(){
+
+}
+
+PreprocessingForPPD <- function(){
+
+}
+
+getPersistentPeaks <- function(){
+
+}
+
+drawPPDBarChart <- function(){
+
+}
+
+drawPPDSurface <- function(){
+
+}
